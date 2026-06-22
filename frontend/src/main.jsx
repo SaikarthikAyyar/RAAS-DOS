2dd40b36f845c7488d1effc1447972a10578491f
SaikarthikAyyar <saikarthik132465@gmail.com>
2026-06-22 16:37:26 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,11 +7,7 @@ import App from './App.jsx'
 
 import './components/salesSurvey/SalesSurvey.css'  // ← add this
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+
 
 import { WorkflowProvider }
 
