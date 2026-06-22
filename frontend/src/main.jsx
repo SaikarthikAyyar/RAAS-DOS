9b7f447440a80ee872710637c54397a9dd7be676
SaikarthikAyyar <saikarthik132465@gmail.com>
2026-06-22 16:07:09 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './styles/sales-survey-glass.css'  // ← add this
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
+
 import { WorkflowProvider }
 
 from "./contexts/WorkflowContext";
