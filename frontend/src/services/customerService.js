90b51d9e2ea07e0017d02ea5214523b2babcf53d
SaikarthikAyyar <narutouzumaki12345pwacbdfe132@gmail.com>
2026-07-10 14:20:12 +0530

    Resolved customer service

--- frontend/src/services/customerService.js
+++ frontend/src/services/customerService.js
@@ -27,49 +27,7 @@ export async function createCustomerRequest(payload){
 
 
 
-// ====================================
-// CREATE CUSTOMER REQUEST
-// ====================================
 
-export async function createCustomerRequest(
-
-payload
-
-){
-
-const response=
-
-await fetch(
-
-`${API}/customer-request`,
-
-{
-
-method:"POST",
-
-headers:{
-
-"Content-Type":
-
-"application/json"
-
-},
-
-body:
-
-JSON.stringify(
-
-payload
-
-)
-
-}
-
-);
-
-return response.json();
-
-}
 
 
 // ====================================
